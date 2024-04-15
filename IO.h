bb100bb2dece897b27898848d66295d8109bb8cd
Busenka <mak.kurlovich@gmail.com>
2024-04-15 15:51:53 +0700

    Class for handling inpung and drawing, uses SDL2

---
+++ IO.h
@@ -0,0 +1,47 @@
+/***********************************************************************
+// File: IO.h
+// Desc: Class for handling inpung and drawing, uses SDL2.
+***********************************************************************/
+
+#ifndef _IO_
+#define _IO_
+
+//---- Includes ----
+
+#include <SDL.h>
+
+//---- Enums ----
+
+enum color {
+  BLACK,
+  RED,
+  GREEN,
+  BLUE,
+  CYAN,
+  MAGENTA,
+  YELLOW,
+  WHITE,
+  COLOR_MAX
+}; // Colors
+
+/*
+======================================================================
+// IO
+======================================================================
+*/
+
+class IO {
+public:
+  IO();
+
+  void DrawRectangle(int pX1, int pY1, int pX2, int pY2, enum color pC);
+  void ClearScreen();
+  int GetScreenHeight();
+  int InitGraph();
+  int PollKey();
+  int GetKey();
+  int IsKeyDown(int pKey);
+  void UpdateScreen();
+};
+
+#endif // !_IO_
